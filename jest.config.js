c4324ea28bbaa89c76c82a7ff1e99abaa9991366
Bruno Nardini <bruno.nardini@resultadosdigitais.com.br>
2020-11-06 11:01:03 -0300

    Fix test configuration

--- jest.config.js
+++ jest.config.js
@@ -123,10 +123,10 @@ module.exports = {
   // runner: "jest-runner",
 
   // The paths to modules that run some code to configure or set up the testing environment before each test
-  // setupFiles: [],
+  setupFiles: ["<rootDir>/jest/setupEnv.js"],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  // setupFilesAfterEnv: [],
+  setupFilesAfterEnv: ["<rootDir>/jest/setupTest.js"],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
--- pages/index.js
+++ pages/index.js
@@ -10,25 +10,28 @@ import Summary from "../components/organisms/Summary";
 import Testimonials from "../components/organisms/Testimonials";
 import Footer from "../components/organisms/Footer";
 import Callout from "../components/organisms/Callout";
+import { ThemeProvider } from "../styles/Theme";
 
 const UDEMY_LINK =
   "https://www.udemy.com/course/react-redux-profissional/?referralCode=D47535C57401C62236B9";
 
 export default function Home() {
   return (
-    <Layout>
-      <Head>
-        <title>React Profissional - Nardini Academy</title>
-      </Head>
-      <Hero courseLink={UDEMY_LINK} />
-      <Summary />
-      <Curriculum />
-      {/* <Stack /> */}
-      <AboutMe />
-      {/* <Projects /> */}
-      <Testimonials />
-      <Callout courseLink={UDEMY_LINK} />
-      <Footer />
-    </Layout>
+    <ThemeProvider>
+      <Layout>
+        <Head>
+          <title>React Profissional - Nardini Academy</title>
+        </Head>
+        <Hero courseLink={UDEMY_LINK} />
+        <Summary />
+        <Curriculum />
+        {/* <Stack /> */}
+        <AboutMe />
+        {/* <Projects /> */}
+        <Testimonials />
+        <Callout courseLink={UDEMY_LINK} />
+        <Footer />
+      </Layout>
+    </ThemeProvider>
   );
 }
